0ef11e9e30cfac18eb638fa8ac641f98bea9a4db
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-19 16:46:28 +0000

    Pass data using props

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,12 +1,13 @@
+import { Expense } from '../App';
 import './ExpenseItem.css';
 
-export default function ExpenseItem() {
+export default function ExpenseItem({title, amount, date}: Expense) {
     return (
     <div className="expense-item">
-        <div>March 28th 2021</div>
+        <div>{date.toDateString()}</div>
         <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$294.67</div>
+            <h2>{title}</h2>
+            <div className='expense-item__price'>${amount}</div>
         </div>
     </div>);
 }
