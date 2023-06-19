92690ef681ca519bdfafaf18a8a6117759f261b3
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-19 16:27:56 +0000

    Added styles to the first component

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,3 +1,12 @@
+import './ExpenseItem.css';
+
 export default function ExpenseItem() {
-    return <h2>Expense item!</h2>;
+    return (
+    <div className="expense-item">
+        <div>March 28th 2021</div>
+        <div className='expense-item__description'>
+            <h2>Car Insurance</h2>
+            <div className='expense-item__price'>$294.67</div>
+        </div>
+    </div>);
 }
